11cbbdad4266d9ee431e5de3c8e48904eecc6f41
Aaron McDaniel <aaron.mcdaniel@gtri.gatech.edu>
2026-07-01 11:50:53 -0400

    reset the autogenerated files for simpler MR

---
+++ src/mixt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=AIsur_x1
+#SBATCH -t 5:00:00
+#SBATCH --gres=gpu:3
+#SBATCH -C "H200"
+#SBATCH --mem 10G
+#SBATCH -c 48
+echo "Launching AIsurBL"
+hostname
+module load gcc/13.2.0
+module load uv
+source ~/.bashrc
+export TOKENIZERS_PARALLELISM=false
+export UV_CACHE_DIR="${TMPDIR:-${SLURM_TMPDIR:-/tmp}}/uv-cache-${SLURM_JOB_ID:-$$}"
+mkdir -p "$UV_CACHE_DIR"
+echo "Using UV cache: $UV_CACHE_DIR"
+uv run python llm_crossover.py '/storage/ice1/2/5/mgullapalli6/2/llm-guided-evolution-infrastructure/sota/Titanic/model.py' '/storage/ice1/2/5/mgullapalli6/2/llm-guided-evolution-infrastructure/sota/Titanic/models/Menghao/model_x.py' '/storage/ice1/2/5/mgullapalli6/2/llm-guided-evolution-infrastructure/sota/Titanic/models/Menghao/model_z.py'  --top_p 0.15   --temperature 0.1 --apply_quality_control 'True' --bit 8
